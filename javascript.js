b8027bff3caea3b5de2aef5e62bbd80adfb4a89e
masaru-yamaguchi-wa <masaru.yamaguchi@woven-planet.global>
2023-01-26 17:31:08 +0900

    add drinkdata,user input function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,69 @@
 //ドリンクの本数計算
-function how_many_drinks(){
-    //現在のドリンクの本数
-}
+// function how_many_drinks() {
+//現在のドリンクの本数
+// }
 
 //投入金額
-function how_much_mpney(){}
+// function how_much_mpney() { }
 
 //Drinkを入れる
-function prompt()
-console.log(userinput)
+// function prompt()
+// console.log(userinput)
+
+
+drink = [
+    {
+        name: "coke",
+        stock: 20,
+        price: 120
+    },
+    {
+        name: "pokari",
+        stock: 12,
+        price: 100
+    },
+    {
+        name: "orange",
+        stock: 60,
+        price: 150
+    },
+    {
+        name: "redbull",
+        stock: 100,
+        price: 210
+    },
+    {
+        name: "tea",
+        stock: 3,
+        price: 100
+    },
+]
+
+for (let i of drink) {
+    console.log(`${i.name}    stock:${i.stock}    price:${i.price} `)
+}
+console.log("Which one do you want?")
+console.log("If you want to buy a drink, put some money in it.")
+
+
+
+function readUserInput(question) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise((resolve, reject) => {
+        readline.question(question, (answer) => {
+            resolve(answer);
+            readline.close();
+        });
+    });
+}
+async function payMoney() {
+    const res = await readUserInput('How much money do you put in? ');
+    console.log(res, "   coin")
+    return res
+};
+
+payMoney()
